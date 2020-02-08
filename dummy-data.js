fdc70593b58d8b962783362fc976c8b06787f661
Timothy Johnson <tjohnson@2ndswing.com>
2020-02-08 14:24:47 -0500

    add data and csv parser

---
+++ dummy-data.js
@@ -0,0 +1,83 @@
+var a = {
+    self: 'a',
+    no: 1,
+    b: true,
+    c: true,
+    d: true,
+    i: true,
+    j: true
+}
+
+var b = {
+    self: 'b',
+    no: 2,
+    a: true,
+    d: true,
+    e: true
+}
+
+var c = {
+    self: 'c',
+    no: 3,
+    d: true,
+    e: true
+}
+
+var d = {
+    self: 'd',
+    no: 4,
+    a: true,
+    c: true,
+    e: true,
+    g: true
+}
+
+var e = {
+    self: 'e',
+    no: 5,
+    a: true,
+    b: true,
+    c: true,
+    d: true
+}
+
+var f = {
+    self: 'f',
+    no: 6,
+    b: true,
+    c: true,
+    d: true
+}
+
+var g = {
+    self: 'g',
+    no: 7,
+    f: true,
+    i: true,
+    e: true
+}
+
+var h = {
+    self: 'h',
+    no: 8,
+    d: true,
+    e: true
+}
+
+var i = {
+    self: 'i',
+    no: 9,
+    g: true,
+    d: true,
+    e: true,
+    j: true
+}
+
+var j = {
+    self: 'j',
+    no: 10,
+    a: true,
+    h: true,
+    f: true,
+    i: true
+}
